e7c7e364e38518f26d403f8e04cdce66673817e2
jinhyung <haneu89@users.noreply.github.com>
2023-01-23 20:11:28 +0900

    인증

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,6 +4,7 @@ exports.verify = async (ctx, next) => {
   var token = ctx.request.headers['token']
   await jwt.verify(token, process.env.APP_KEY, async  (error, decoded) => {
     if(error) {
+      ctx.status = 401;
       ctx.body = '로그인을 해야합니다';
       return;
     }
